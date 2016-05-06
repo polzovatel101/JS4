05297a3857cc6991b37ea59f64bc678da1669341
Илья Яновой <polzovatel101@mail.ru>
2016-05-06 20:40:53 +0300

    JS4 HW upd

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
  */
 $(function () {
 
+    var $text = ('.text');
+
     function showContent($el) {
         var index = $el.index();
 
-        $('.text').eq(index).addClass('show').siblings().addClass('text').removeClass('show');
+        $text.eq(index).addClass('show').siblings().addClass('text').removeClass('show');
     }
 
     function showHelp($elem) {
@@ -38,5 +40,18 @@ $(function () {
 
     $inputField.on('mouseleave', function () {
         hideHelp($(this));
+    });
+
+    $('.help').on('click', function () {
+        for(var i = 0; i < $inputField.length; i++ ){
+            console.log($inputField.eq(i).attr('data-title'));
+            showHelp($inputField.eq(i));
+        }
+        setTimeout(function() {
+            for(var i = 0; i < $inputField.length; i++)
+            {
+                hideHelp($inputField.eq(i));
+            }
+        },5000)
     })
 });
